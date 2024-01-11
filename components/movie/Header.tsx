c849ac390d58ee63845247fbf33d84af48b1ee84
Sang Nguyen <131627842+Charoneeee@users.noreply.github.com>
2024-01-11 14:38:34 +0700

    Update Header (Add Read More - Read Less)

--- components/movie/Header.tsx
+++ components/movie/Header.tsx
@@ -1,5 +1,5 @@
 import { Box, Button, Stack, Typography } from "@mui/material";
-import React from "react";
+import React, { useState } from "react"; // Import useState
 import AddCircleIcon from "@mui/icons-material/AddCircle";
 import TurnedInNotIcon from "@mui/icons-material/TurnedInNot";
 import { useRouter } from "next/router";
@@ -8,6 +8,25 @@ import axios from "axios";
 import { MovieList } from "@/models/Movie"; // Import Movie instead of MovieList
 
 type Props = {};
+
+// Create a reusable Read More/Less component
+const ExpandableText = ({ children, descriptionLength }) => {
+  const fullText = children;
+  // Set the initial state of the text to be collapsed
+  const [isExpanded, setIsExpanded] = useState(false);
+  // This function is called when the read more/less button is clicked
+  const toggleText = () => {
+    setIsExpanded(!isExpanded);
+  };
+  return (
+    <p className="text">
+      {isExpanded ? fullText : `${fullText.slice(0, descriptionLength)}...`}
+      <span onClick={toggleText} className="toggle-button">
+        {isExpanded ? "Read less" : "Read more"}
+      </span>
+    </p>
+  );
+};
 
 const Header = (props: Props) => {
   const router = useRouter();
@@ -20,52 +39,55 @@ const Header = (props: Props) => {
 
   return (
     <>
-      <Stack gap={4} direction={"row"}>
-        <Box>
-          {data?.results.map((movie) => (
-            <Stack key={movie.id} spacing={2}>
-              <Stack>
-                <Box
-                  component="img"
-                  src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                  width={"100%"}
-                />
-              </Stack>
+      <Stack gap={4}>
+        {data?.results.map((movie) => (
+          <Stack key={movie.id} spacing={2}>
+            <Stack>
               <Box
-                sx={{
-                  background:
-                    "linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.7))",
-                }}
-              >
-                <Box fontSize={"30px"}>{movie.title}</Box>
-                <Stack direction={"row"} spacing={2}>
-                  <Box>{movie.release_date}</Box>
-                  <Box>
-                    <Typography>genre</Typography>
-                  </Box>
-                </Stack>
-                <Box>{movie.overview}</Box>
-                <Stack direction={"row"} spacing={3}>
-                  <Button
-                    sx={{ backgroundColor: "green", width: "50%" }}
-                    variant="contained"
-                    startIcon={<AddCircleIcon />}
-                  >
-                    Play Now
-                  </Button>
-                  <Button
-                    color="inherit"
-                    sx={{ width: "50%" }}
-                    variant="outlined"
-                    startIcon={<TurnedInNotIcon />}
-                  >
-                    Add watchlist
-                  </Button>
-                </Stack>
-              </Box>
+                component="img"
+                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                width={"100%"}
+              />
             </Stack>
-          ))}
-        </Box>
+            <Box
+              sx={{
+                background:
+                  "linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.7))",
+              }}
+            >
+              <Box fontSize={"30px"}>{movie.title}</Box>
+              <Stack direction={"row"} spacing={2}>
+                <Box>{movie.release_date}</Box>
+                <Box>
+                  <Typography>genre</Typography>
+                </Box>
+              </Stack>
+              <Box>
+                {/* Only show 100 characters in the beginning */}
+                <ExpandableText descriptionLength={100}>
+                  {movie.overview}
+                </ExpandableText>
+              </Box>
+              <Stack direction={"row"} spacing={3}>
+                <Button
+                  sx={{ backgroundColor: "green", width: "50%" }}
+                  variant="contained"
+                  startIcon={<AddCircleIcon />}
+                >
+                  Play Now
+                </Button>
+                <Button
+                  color="inherit"
+                  sx={{ width: "50%" }}
+                  variant="outlined"
+                  startIcon={<TurnedInNotIcon />}
+                >
+                  Add watchlist
+                </Button>
+              </Stack>
+            </Box>
+          </Stack>
+        ))}
       </Stack>
     </>
   );

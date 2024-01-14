691eba6ffd2bca3f1f47a29a0c446711431122ff
DESKTOP-6KHCC5V\Ron <truong.pn.62cntt@ntu.edu.vn>
2024-01-14 22:17:47 +0700

    Trường Movie Card

--- components/movie/MovieCard.tsx
+++ components/movie/MovieCard.tsx
@@ -31,7 +31,12 @@ const MovieCard = () => {
         sx={{ overflowX: "auto" }} // Thêm kiểm soát tràn ngang
       >
         {data?.results.map((movie) => (
-          <Stack key={movie.id} direction="column" alignItems="center">
+          <Stack
+            key={movie.id}
+            direction="column"
+            alignItems="center"
+            position={"relative"}
+          >
             <Box
               component="img"
               src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
@@ -40,15 +45,33 @@ const MovieCard = () => {
               sx={{ borderRadius: "10%" }}
               onClick={() => handleDetailClick(movie.id)}
             />
-            <Box sx={{ flexGrow: 1, color: "white", marginTop: "20px" }}>
-              {movie.title}
-            </Box>
-            <Stack direction="row" alignItems="center">
-              <StarRateIcon sx={{ color: "yellow" }}></StarRateIcon>
-              <Box sx={{ color: "white" }}>
-                {(movie.vote_average * 0.5).toFixed(1)}/5
+            <Box
+              position={"absolute"}
+              zIndex={"1"}
+              bottom={"20px"}
+              left={"30px"}
+            >
+              <Box sx={{ flexGrow: 1, color: "white", marginTop: "20px", marginLeft:"5px", fontSize:"20px"}}>
+                {movie.title}
               </Box>
-            </Stack>
+              <Stack direction="row" alignItems="center">
+                <StarRateIcon sx={{ color: "yellow" }}></StarRateIcon>
+                <Box sx={{ color: "white" }}>
+                  {(movie.vote_average * 0.5).toFixed(1)}/5
+                </Box>
+              </Stack>
+            </Box>
+            <Box
+              position={"absolute"}
+              zIndex={"0"}
+              top={"400px"}
+              sx={{
+                background:
+                  "linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1))",
+                width: "350px",
+                height: "100px",
+              }}
+            ></Box>
           </Stack>
         ))}
       </Stack>

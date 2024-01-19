29e2e51fb2ce81655d92c1c5931845296f5430d7
Minhminle <voleminhnghia176202@gmail.com>
2024-01-19 15:30:20 +0700

    [Detail] build Similar Movies

--- pages/detail/movie/[id].tsx
+++ pages/detail/movie/[id].tsx
@@ -15,6 +15,8 @@ import { ReactElement } from "react";
 import { Movie } from "@/models/Movie";
 import { Rating, Chip } from "@mui/material";
 import Footter from "@/components/movie/Footer";
+import React, { useState } from "react";
+import StarIcon from "@mui/icons-material/Star";
 
 export interface Cast {
   id: number;
@@ -26,127 +28,92 @@ export interface Cast {
 const MovieDetail = () => {
   const router = useRouter();
   const { id } = router.query;
-
-  const fetcher = (url: string) =>
-    axios.get(url).then((response) => response.data);
   const { data, error } = useSWR<Movie>(
-    `/movie/${id}?append_to_response=credits`,
-    fetcher
+    `/movie/${id}?append_to_response=credits,similar`
   );
+  const { data: similarMoviesData, error: similarMoviesError } = useSWR(
+    `/movie/${id}/similar`
+  );
+  const _letterStyles = {
+    color: "white",
+    fontWeight: "700",
+  };
+  const handleDetailClick = (movieId: string) => {
+    router.push(`/detail/movie/${movieId}`);
+  };
+  const [expandedOverview, setExpandedOverview] = useState<string | null>(null);
 
   if (error) return <div>Error loading movie details</div>;
   if (!data) return <div>Loading...</div>;
   return (
     <>
-      <Stack gap={4} sx={{ backgroundColor: "black", color: "white" }}>
-        <Box
-          component="img"
-          src={`https://image.tmdb.org/t/p/w500${data.poster_path}`}
-          alt={data.title}
-          style={{
-            boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.3)",
-          }}
-        />
-        <Box
-          sx={{
-            margin: "20px",
-          }}
-        >
-          <Typography variant="h3" sx={{}}>
-            {data.title}
-          </Typography>
-          <Typography
-            variant="body1"
-            sx={{ fontSize: "18px", color: "#555", marginBottom: "20px" }}
+      <Typography variant="h4" sx={{ ..._letterStyles, padding: "10px" }}>
+        Similar Movies:
+      </Typography>
+
+      <Stack
+        direction="row"
+        sx={{ overflowX: "auto" }} // Thêm kiểm soát tràn ngang
+      >
+        {similarMoviesData?.results.map((movie) => (
+          <Box
+            key={movie.id}
+            sx={{
+              margin: "10px",
+              backgroundColor: "black",
+              color: "white",
+            }}
           >
-            {data.overview}
-          </Typography>
-          <Stack direction={"row"}>
-            <Rating
-              name="movie-rating"
-              value={data.vote_average ? data.vote_average / 2 : 0}
-              precision={0.5}
-              readOnly
-              sx={{ color: "#FFD700", fontSize: 24 }}
+            <CardMedia
+              style={{
+                borderRadius: "20px",
+                height: "180px",
+                width: "300px",
+              }}
+              onClick={() => handleDetailClick(movie.id)}
+              component="img"
+              alt={movie.title}
+              image={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
             />
-            <Stack direction="row" spacing={3}>
-              <Typography variant="h6" sx={{ color: "yellow" }}>
-                {(data.vote_average * 0.5).toFixed(1)}
+            <CardContent>
+              <Typography
+                variant="h5"
+                onClick={() => handleDetailClick(movie.id)}
+              >
+                {expandedOverview === movie.title
+                  ? movie.title
+                  : movie.title.length > 15
+                  ? `${movie.title.slice(0, 15)}...`
+                  : movie.title}
               </Typography>
-              <Typography variant="h5" sx={{ color: "#d32f2f" }}>
-                Votes: {data.vote_count}
-              </Typography>
-            </Stack>
-          </Stack>
-          <Typography variant="h5" sx={{ color: "#42a5f5", marginTop: "10px" }}>
-            Genres:
-            {data.genres?.map((genre) => (
-              <Chip
-                key={genre.id}
-                label={genre.name}
-                sx={{
-                  margin: "3px",
-                  backgroundColor: "#673ab7",
-                  color: "white",
-                  marginRight: "5px",
-                  marginBottom: "5px",
-                }}
-              />
-            ))}
-          </Typography>
-          {/* <Typography variant="h5" sx={{ color: "#1de9b6", marginTop: "10px" }}>
-            Cast:
-            {data.credits?.cast?.slice(0, 5).map((actor) => (
-              <Chip
-                key={actor.id}
-                label={`${actor.name} as ${actor.character}`}
-                sx={{
-                  margin: "3px",
-                  backgroundColor: "#66bb6a",
-                  color: "white",
-                  marginRight: "5px",
-                  marginBottom: "5px",
-                }}
-              />
-            ))}
-          </Typography> */}
-          <Typography variant="h5" sx={{ color: "#1de9b6", marginTop: "10px" }}>
-            Cast:
-            <Grid container spacing={2}>
-              {data.credits?.cast?.slice(0, 5).map((actor) => (
-                <Grid item key={actor.id} xs={12} sm={6} md={4} lg={3}>
-                  <Box
-                    sx={{
-                      color: "white",
-                      padding: "10px",
-                      borderRadius: "8px",
-                    }}
+              <Stack direction={"row"} spacing={2}>
+                <Box
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                  }}
+                >
+                  <StarIcon style={{ marginRight: "4px", color: "#ffeb3b" }} />
+                  <Typography variant="body2">
+                    {(movie.vote_average * 0.5).toFixed(1)}
+                  </Typography>
+                </Box>
+
+                {data.genres?.map((genre) => (
+                  <Typography
+                    key={genre.id}
+                    variant="h6"
+                    style={{ marginTop: "-1px" }}
+                    sx={{ color: "#9e9e9e" }}
                   >
-                    <Stack direction="row" alignItems="center">
-                      <Avatar
-                        src={`https://image.tmdb.org/t/p/w500${actor.profile_path}`}
-                        alt={actor.name}
-                        sx={{ marginRight: "10px" }}
-                      />
-                      <Box>
-                        <Typography
-                          variant="subtitle1"
-                          sx={{ fontWeight: "bold" }}
-                        >
-                          {actor.name}
-                        </Typography>
-                        <Typography variant="body2">{`${actor.character}`}</Typography>
-                      </Box>
-                    </Stack>
-                  </Box>
-                </Grid>
-              ))}
-            </Grid>
-          </Typography>
-        </Box>
+                    {genre.name}
+                  </Typography>
+                ))}
+              </Stack>
+            </CardContent>
+          </Box>
+        ))}
       </Stack>
-      <Stack></Stack>
-      <Footter></Footter>
     </>
   );
 };

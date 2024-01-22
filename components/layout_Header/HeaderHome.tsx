cf57f31915d5530d264d698212f9dfb7b5dc7f8d
DESKTOP-6KHCC5V\Ron <truong.pn.62cntt@ntu.edu.vn>
2024-01-23 00:04:06 +0700

    sucessful Login

--- components/layout_Header/HeaderHome.tsx
+++ components/layout_Header/HeaderHome.tsx
@@ -4,6 +4,7 @@ import SearchIcon from "@mui/icons-material/Search";
 import MenuIcon from "@mui/icons-material/Menu";
 import { Styles } from "@/stylescomponents/style";
 import { useRouter } from "next/router";
+import AvatarView from "../movie/AvatarView";
 const HeaderHome = () => {
   const findLink = "/detail/Find";
   const router = useRouter();
@@ -15,14 +16,14 @@ const HeaderHome = () => {
             <Box component="img" src="/icons/Logo.svg" />
             <Box component="img" src="/icons/SaintStream.svg" />
           </Stack>
-          <Stack direction="row" spacing={3}>
+          <Stack direction="row" spacing={1} alignItems="center">
             <SearchIcon
               onClick={() => {
                 router.push(findLink);
               }}
               sx={Styles._iconheaderhome}
             />
-            <MenuIcon sx={Styles._iconheaderhome} />
+           <AvatarView  ></AvatarView>
           </Stack>
         </Stack>
       </Box>
